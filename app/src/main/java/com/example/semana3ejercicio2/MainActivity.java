08779244fb2db4a7fa06bba3eb3857a047ecf954
luis antonio amaya villalta <amayantonio@hotmail.com>
2022-02-05 08:57:16 -0600

    agregando controles

--- app/src/main/java/com/example/semana3ejercicio2/MainActivity.java
+++ app/src/main/java/com/example/semana3ejercicio2/MainActivity.java
@@ -2,21 +2,34 @@ package com.example.semana3ejercicio2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    public  String dato="Si";
 
     public EditText valor1, valor2;
     public Button sumar;
     public TextView vista;
-    public ImageButton imgButon;
+    public ImageButton imgButon, play;
+    MediaPlayer sound;
+
+    private CheckBox AceptaTC;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,45 @@ public class MainActivity extends AppCompatActivity {
         sumar=(Button) findViewById(R.id.btnsumar);
         vista=(TextView) findViewById(R.id.txtVer);
 imgButon=(ImageButton) findViewById(R.id.botonImage);
+play=(ImageButton)findViewById(R.id.play);
+
+
+        AceptaTC=(CheckBox)findViewById(R.id.checkBox);
+
+
+
+
+
+
+play.setOnTouchListener(new View.OnTouchListener() {
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        if(event.getAction() == MotionEvent.ACTION_UP){
+            play.setImageResource(R.drawable.dos);
+            sound.start();
+            return true;
+        }else if(event.getAction() == MotionEvent.ACTION_DOWN){
+            play.setImageResource(R.drawable.uno);
+            return true;
+        }
+
+
+        return false;
+    }
+});
+
+
+
+
+
+
+        sound=MediaPlayer.create(getApplicationContext(), R.raw.birds1);
+
+
+
+
+
 
 
 
@@ -41,16 +93,13 @@ imgButon.setOnTouchListener(new View.OnTouchListener() {
         if(event.getAction() == MotionEvent.ACTION_UP){
             imgButon.setImageResource(R.drawable.dos);
 
-            String dato1=valor1.getText().toString();
-            int dat1=Integer.parseInt(dato1);
 
-            String dato2=valor2.getText().toString();
-            int dat2=Integer.parseInt(dato2);
-
-            int sumaTotal=dat1+dat2;
-
-            vista.setText("La suma es: "+ sumaTotal);
-
+            if (AceptaTC.isChecked()==true) {
+                sumar.setEnabled(true);
+                Log.d (dato, "se llama a la función onCreate");
+            }else{
+                sumar.setEnabled(false);
+            }
 
 
 
@@ -63,6 +112,15 @@ imgButon.setOnTouchListener(new View.OnTouchListener() {
         return false;
     }
 });
+
+
+
+
+
+
+
+
+
 
     }
 
@@ -81,11 +139,21 @@ imgButon.setOnTouchListener(new View.OnTouchListener() {
 
         vista.setText("La suma es: "+ sumaTotal);
 
-String algo="sfsdfdsfds";
-        Toast.makeText(getApplicationContext(), algo,Toast.LENGTH_LONG).show();
 
-     //   imgButon.setBackgroundResource(R.drawable.car);
 
+       int ale=getRandomNumber(40,70);
+       String algo=String.valueOf(ale);
+
+
+       Toast.makeText(getApplicationContext(), algo,Toast.LENGTH_LONG).show();
+
+
+    }
+
+
+
+    public static int getRandomNumber(int min, int max) {
+        return (new Random()).nextInt((max - min) + 1) + min;
     }
 
 
